9fe88cc53ab2526e1c706f13860b8aed90888e9e
gopalbasak1 <gopalbasak1223@gmail.com>
2024-05-06 08:06:47 +0600

    connection mongodb and added a user

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -15,3 +16,49 @@ const corsOptions = {
 //middleware
 app.use(cors(corsOptions));
 app.use(express.json());
+
+
+//SoloServer
+//eATpL1KXVSVYF0DI
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhiqbma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      //await client.connect();
+
+    
+
+
+
+
+
+
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      //await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from SoloSphere Server...')
+})
+
+app.listen(port, ()=> console.log(`Server running on port: ${port}`));
