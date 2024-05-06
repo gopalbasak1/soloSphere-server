515e54a002545fe95bb7c66a219eb8808744ad6d
gopalbasak1 <gopalbasak1223@gmail.com>
2024-05-06 20:21:43 +0600

    jobs data update delete and bid create to the mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -18,8 +18,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 
-//SoloServer
-//eATpL1KXVSVYF0DI
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhiqbma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -38,8 +36,71 @@ const client = new MongoClient(uri, {
       // Connect the client to the server	(optional starting in v4.7)
       //await client.connect();
 
-    
+      const jobsCollection = client.db('SoloSphere').collection('jobs');
+      const bidsCollection = client.db('SoloSphere').collection('bids');
 
+    
+      //Get all jobs data from db
+
+      app.get('/jobs', async(req, res) => {
+        const result = await jobsCollection.find().toArray();
+        res.send(result)
+      })
+
+      //Get a single job data from db using job id
+      app.get('/job/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await jobsCollection.findOne(query);
+        res.send(result);
+      })
+
+
+      //Save a job data in db
+      app.post('/job', async(req, res) =>{
+        const jobData = req.body;
+        const result = await jobsCollection.insertOne(jobData);
+        res.send(result);
+      })
+
+      //Save all jobs posted by a specific user
+      app.get('/jobs/:email', async(req, res)=>{
+        const email = req.params.email;
+        const query = {'buyer.email': email};
+        const result = await jobsCollection.find(query).toArray();
+        res.send(result);
+      })
+
+      //delete a job data from db
+      app.delete('/job/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await jobsCollection.deleteOne(query);
+        res.send(result);
+      })
+
+      //update a job in db
+      app.put('/job/:id', async(req, res) => {
+        const id = req.params.id;
+        const jobData = req.body;
+        const query = {_id: new ObjectId(id)}
+        const options = {upsert: true}
+        const updateDoc = {
+          $set:{
+            ...jobData,
+          }
+        }
+        const result = await jobsCollection.updateOne(query, updateDoc, options);
+        res.send(result);
+      });
+
+
+      //Save a bid data in db
+      app.post('/bid', async(req, res) =>{
+        const bidData = req.body;
+        const result = await bidsCollection.insertOne(bidData);
+        res.send(result);
+      })
 
 
 
